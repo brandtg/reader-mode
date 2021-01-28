6576d0483c7629932beaa97717a7ced178dd62e0
Greg Brandt <greg@emergencelabs.ai>
2021-01-27 21:09:18 -0800

    Tweak fonts

--- readerMode.js
+++ readerMode.js
@@ -144,10 +144,13 @@ function renderReaderModeNewTab() {
         margin-right: auto;
         padding: 1em;
         font-size: 14pt;
-        font-family: serif;
+        font-family: -apple-system, BlinkMacSystemFont,
+            "Segoe UI", "Roboto", "Oxygen",
+            "Ubuntu", "Cantarell", "Fira Sans",
+            "Droid Sans", "Helvetica Neue", sans-serif;
       }
       p {
-        line-height: 1.6;
+        line-height: 1.2;
       }
       a {
         color: #5E81AC;
